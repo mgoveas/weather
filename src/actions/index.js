a55c8d17b66c867bd670fec3a168843a9379794e
Maria Goveas <Maria_Goveas@intuit.com>
2018-01-26 09:14:08 -0800

    course work

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'a034c2ad2cfdd268edb7f97f6a4d5ea1';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
